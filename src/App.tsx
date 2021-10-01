1b3f1aa2bf5628b190ec787bd610fa3c80513cc7
cshov <cshovplaysultimate@gmail.com>
2021-09-30 23:56:28 -0400

    initial commit for new react app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import logo from './pennywise.svg';
 import './App.css';
 
 function App() {
@@ -7,18 +8,24 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>October Horror Binge</h1>
+        <img src={logo} className="App-logo" alt="logo" />
+        {/*<a*/}
+        {/*  className="App-link"*/}
+        {/*  href="https://reactjs.org"*/}
+        {/*  target="_blank"*/}
+        {/*  rel="noopener noreferrer"*/}
+        {/*>*/}
+        {/*  Learn React*/}
+        {/*</a>*/}
       </header>
+        <div>
+            <p>
+                It's time to watch some fucking horror movies.
+            </p>
+        </div>
+
+
     </div>
   );
 }
