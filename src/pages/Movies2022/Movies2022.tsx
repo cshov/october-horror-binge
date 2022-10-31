7397fab9ab8c5ea9240b49a49301de08a7149edc
cshov <cshovplaysultimate@gmail.com>
2022-10-31 16:13:22 -0400

    trying to kick off deployment

--- src/pages/Movies2022/Movies2022.tsx
+++ src/pages/Movies2022/Movies2022.tsx
@@ -13,16 +13,19 @@ function Movies2022() {
             {moviesByDayArray.map(({ day, movies }, i) => {
                 return (
                     <div className="dayPanel">
+
                         <div className="dayMonthLabel">
                             <span className="spacer"></span>
                             <span className="monthLabel">OCT</span>
                             <span className="dayLabel">{day}</span>
                         </div>
+
                         <div className="movies">
                             {movies.map((movie: Movie, i) => (
                                 <p><a href={"https://www.imdb.com/title/"+ movie.imdbId + "/"}><strong>{movie.name}</strong> ({movie.year})</a></p>
                             ))}
                         </div>
+
                     </div>
                 );
             })}
