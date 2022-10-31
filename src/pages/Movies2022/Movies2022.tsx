d354d4cb548c89bc5036de63d24cce1cce2cedb1
cshov <cshovplaysultimate@gmail.com>
2022-10-31 15:32:55 -0400

    updating 2022 list styles

--- src/pages/Movies2022/Movies2022.tsx
+++ src/pages/Movies2022/Movies2022.tsx
@@ -10,18 +10,14 @@ function Movies2022() {
     return (
         <div>
             <p>This is a list of all the horror movies I've watched in October 2022. So far, that list includes <strong>{getMoviesTotal2022()}</strong> different movies.</p>
-
             {moviesByDayArray.map(({ day, movies }, i) => {
                 return (
                     <div className="dayPanel">
-
                         <div className="dayMonthLabel">
                             <span className="spacer"></span>
                             <span className="monthLabel">OCT</span>
                             <span className="dayLabel">{day}</span>
                         </div>
-
-
                         <div className="movies">
                             {movies.map((movie: Movie, i) => (
                                 <p><a href={"https://www.imdb.com/title/"+ movie.imdbId + "/"}><strong>{movie.name}</strong> ({movie.year})</a></p>
